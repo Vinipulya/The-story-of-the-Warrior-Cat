8917b642165db102743bea7a417843383d74968d
Rentox <toster2112@yandex.com>
2025-01-13 20:20:33 +0300

    Создал поле

--- cat.py
+++ cat.py
@@ -4,12 +4,21 @@ import sys
 import pygame
 
 pygame.init()
-size = width, height = 640, 320
+pygame.key.set_repeat(200, 70)
+size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
+FPS = 50
+clock = pygame.time.Clock()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('Sprites', name)
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -24,26 +33,134 @@ def load_image(name, colorkey=None):
     return image
 
 
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 70
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('red'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': pygame.transform.scale(load_image('Tentacle.png'), (50, 50)),
+    'empty': pygame.transform.scale(load_image('Tiles.png'), (50,50))
+}
+player_image = load_image('mar.png')
+
+tile_width = tile_height = 50
+
+# основной персонаж
+player = None
+
+# группы спрайтов
 all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+wall_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 
-for j in range(height // 32):
-    for k in range(width // 32):
-        tiles_image = load_image("Tiles.png")
-        tiles = pygame.sprite.Sprite(all_sprites)
-        tiles.image = tiles_image
-        tiles.rect = tiles.image.get_rect()
-        tiles.rect.x = k * 32
-        tiles.rect.y = j * 32
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        if tile_type == 'wall':
+            super().__init__(tiles_group, all_sprites, wall_group)
+        else:
+            super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.tile_type = tile_type
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.tile_type = "player"
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+
+start_screen()
+player, level_x, level_y = generate_level(load_level('map.txt'))
 running = True
-fps = 50
-clock = pygame.time.Clock()
+STEP = 10
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((0, 0, 0))
-    all_sprites.draw(screen)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.rect.x -= STEP
+                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                    player.rect.x += STEP
+            if event.key == pygame.K_RIGHT:
+                player.rect.x += STEP
+                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                    player.rect.x -= STEP
+            if event.key == pygame.K_UP:
+                player.rect.y -= STEP
+                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                    player.rect.y += STEP
+            if event.key == pygame.K_DOWN:
+                player.rect.y += STEP
+                if pygame.sprite.groupcollide(player_group, wall_group, False, False):
+                    player.rect.y -= STEP
+
+    screen.fill(pygame.Color(0, 0, 0))
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
-pygame.quit()
+
+    clock.tick(FPS)
+terminate()
